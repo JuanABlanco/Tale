ed32dd8681e2aedc4ffbbff3193a00372eeffd54
Juan Blanco <juan15597l@gmail.com>
2021-09-11 04:08:40 +0200

    Navbar listo en todo sentido menos refractor y utilidad

--- tale/src/App.js
+++ tale/src/App.js
@@ -1,16 +1,17 @@
-import NavBar from './components/shared/navBar';
-import NavItem from './components/shared/navItem';
+/*import NavBar from './components/shared/navBar';
+import NavItem from './components/shared/navItem';*/
+import Nav from './components/shared/Nav'
 
- 
-function App() {
-  return (
-    <div className=" h-screen w-screen bg-gray-800">
-      <NavBar>
+/* <NavBar>
         <NavItem href="/featured" isActive>Featured</NavItem>
         <NavItem href="/featured">Store</NavItem>
         <NavItem href="/featured">K-store</NavItem>
         <NavItem href="/featured">K-store</NavItem>
-      </NavBar>
+      </NavBar>*/
+function App() {
+  return (
+    <div className=" h-screen w-screen bg-gray-800">
+      <Nav></Nav>
     </div>
   );
 }
